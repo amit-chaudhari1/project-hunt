c0ef66b85849e5be8f52d9a8fcf90fead052d41d
amitchaudhari9121 <amitchaudhari9121@gmail.com>
2021-02-16 13:34:30 +0530

    Added Database to the app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'projectHunt',
+      username: 'hotshot_dev',
+      password: 'passwd',
+      entities: [], //TODO: Create entities and document them.
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
